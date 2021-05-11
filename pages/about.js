45cccafc505314b30e5208a166e52320f0f980ad
Kenny J <k-tech19@Kennys-Mac-mini.local>
2021-05-11 14:49:41 -0400

    intial set up

---
+++ pages/about.js
@@ -0,0 +1,9 @@
+export default function AboutPage() {
+    return (
+        <div>
+            <h2>Hello about me page here</h2>
+            <p> This is an app to find the latest DJ events and other musical events around town</p>
+            <p>Verison 1.00</p>
+        </div>
+    )
+}
---
+++ pages/events/index.js
@@ -0,0 +1,12 @@
+
+export default function EventPage() {
+    return (
+        <div> 
+            <ul> 
+                <li> Event one </li>
+                <li> Event two </li>
+                <li> Event three </li>
+            </ul>
+        </div>
+    )
+}
